e0a967bbec7ee6a1db102d086318d93995fc1e40
Nerwus43 <michalpalus43@gmail.com>
2025-08-06 15:13:40 +0200

    lang

--- js/load_translation.js
+++ js/load_translation.js
@@ -7,11 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
     translatePage(lang);
     const flagaPolski = document.getElementById("poland_flag");
     const flagaNiemiec = document.getElementById("germany_flag");
-
+    const flagaWielkiejBrytani = document.getElementById("uk_flag");
+    const flagaSlaska = document.getElementById("silesia_flag");
     if (flagaPolski)
       flagaPolski.style.border = lang === "pl" ? "2px solid red" : "none";
     if (flagaNiemiec)
       flagaNiemiec.style.border = lang === "de" ? "2px solid red" : "none";
+    if (flagaWielkiejBrytani)
+      flagaWielkiejBrytani.style.border = lang === "uk" ? "2px solid red" : "none";
+    if (flagaSlaska)
+      flagaSlaska.style.border = lang === "si" ? "2px solid red" : "none";
   } else {
     translatePage("pl"); // domyślny język
   }
