0d7d8cdcf8671c5ac1edff5e9682574502697289
Nerwus43 <michalpalus43@gmail.com>
2025-08-06 15:17:08 +0200

    statystyki

--- js/translations.js
+++ js/translations.js
@@ -114,7 +114,7 @@ const dictionary = {
     Temperatura: "Tymperatura",
     Światło: "Światło",
     Biuro: "Biōro",
-    Statystyki: "Statistiken",
+    Statystyki: "Statystyki",
     piętro: "piyntro",
     harmonogram: "ôrganizacja",
     raporty: "raporty",
