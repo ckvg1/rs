eec3d073504cddbb1fce9472e81552fafd20bbf0
Nerwus43 <michalpalus43@gmail.com>
2025-08-06 10:53:47 +0200

    ciemny

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,27 +1,15 @@
 const local = localStorage.getItem("theme");
 if(local === "ciemny-motyw"){
-  document.querySelectorAll("body, header, .modal-content, .hamburger, .tab-button, .oaerror, section").forEach((el) => {
+  document.querySelectorAll("body, header, .modal-content, .hamburger, .tab-button, .oaerror, section, .przycisk, input, select, .icons_div").forEach((el) => {
     el.classList.add("ciemny");
   });
 
 
   document.getElementById("settings_icon").classList.add("ciemny");
 
+  document.querySelector(".logo").src = "logo-dark.svg"
   document.getElementById("settings_icon").src = "./img_main/settings-dark.png";
-  document.getElementById("dark-mode").src = "img_main/dark-mode-dark.png";
-
-  ramka = document.getElementById("ramka");
-  doc = ramka.contentWindow.document;
-  img = doc.getElementById("light_off");
-  // img.style.width = "1000px"
-  if(img.src == 'img/light_off.png'){
-    img.src = 'img/light_off-dark.png'
-  }
-  else{
-    img.src ='img/light_off-dark.png'
-  }
-  // document.getElementById("light_off").src = "img/light_off-dark.png"
-  // document.getElementById("main_image").src = "./img_main/swiatla-bg-dark2.png";
+  document.getElementById("dark-mode").src = "img_main/dark-mode-dark.png"
 }
 
 function darkMode(){
@@ -30,7 +18,6 @@ function darkMode(){
   body.forEach((el) => {
     el.classList.toggle("ciemny");
   });
-
 
   if (document.body.classList.contains("ciemny")){
       localStorage.setItem("theme", "ciemny-motyw");
