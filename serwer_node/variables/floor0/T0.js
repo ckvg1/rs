5bf893e2c21b2135a8ab50def78a96a584fbda5b
blukasik77 <kfckvgjv59@gmail.com>
2025-08-08 14:30:58 +0200

    serwer, komentarze, osobny plik ze sterowaniem

---
+++ serwer_node/variables/floor0/T0.js
@@ -0,0 +1,12 @@
+//temperatura parter
+
+// nie wiem jakie adresy powinny być, więc zostawiam takie jak w T2.js 
+module.exports = {
+  t0_1: "DB9,REAL44",
+  t0_2: "DB9,REAL48",
+  t0_3: "DB9,REAL52",
+  t0_4: "DB9,REAL56",
+  t0_5: "DB9,REAL60",
+  t0_6: "DB9,REAL64",
+  t0_7: "DB9,REAL68",
+};
---
+++ serwer_node/variables/floor1/B1_in.js
@@ -0,0 +1,11 @@
+//wejscia rolet 1 pietro
+module.exports = {
+  wej_b1_r4_1: "DB11,X2.5",
+  wej_b1_r4_2: "DB11,X2.6",
+  wej_b1_r5_1: "DB11,X2.7",
+  wej_b1_r5_2: "DB11,X3.0",
+  wej_b1_r6_1: "DB11,X3.1",
+  wej_b1_r6_2: "DB11,X3.2",
+  wej_b1_r7_1: "DB11,X3.3",
+  wej_b1_r7_2: "DB11,X3.4",
+};
